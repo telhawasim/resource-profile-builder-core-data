ae70427516856b062f95268d00e9719bdcb907a9
telhawasim <telhawasim@gmail.com>
2023-10-29 13:21:31 +0500

    initial commit

---
+++ Resource Profile Builder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Resource Profile Builder
+//
+//  Created by Telha Wasim on 29/10/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
